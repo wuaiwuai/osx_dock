f7025461f2d26dd8e157639744bc3c4b526c3e5e
Jon Dang <jondang@gannett.com>
2014-01-15 17:46:54 -0500

    unfinished app scale relative to mouse position

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@
 // get position of app
 var appWidth = $( '.app' ).width();
 var test = $( '.app' ).position().left;
-console.log(test + (appWidth*.5));
+var appCenter = test + (appWidth*.5)
 
 $( '#dock-container' ).mousemove(function(e) {
 	var x = e.pageX - this.offsetLeft;
     var y = e.pageY - this.offsetTop;
     $('#debug').html(x +', '+ y);
+
+    var d = x-appCenter;
+	// $( '.app' ).stop().transition({ scale: d*2, duration: 0 });
+    console.log( d );
+    // if (d > 1) {
+    // 	$( '.app' ).stop().transition({ scale: d*2, duration: 0 });
+    // }
+
 });
 
 // $( '.app' ).on({
